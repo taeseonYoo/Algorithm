257bdf65138887be0d50286bcb085e3646c661d9
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2024-12-19 22:09:29 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 28.75 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,76 @@
+class Solution {
+    
+    int [][] list;
+    int [] point;
+    
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        
+        int n = friends.length;
+        
+        list = new int[n][n];
+        point = new int[n];
+        
+        for(int i=0;i<gifts.length;i++){
+            String[] names = gifts[i].split(" ");
+            
+            int a=0,b=0;
+            
+            for(int j=0;j<n;j++){
+                if(names[0].equals(friends[j])){
+                    a = j;
+                }
+                if(names[1].equals(friends[j])){
+                    b = j;
+                }
+            }
+            
+            list[a][b] += 1;
+            
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                point[i] += list[i][j];
+                point[j] -= list[i][j];
+            }
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.print(list[i][j]+" ");
+        //     }
+        //     System.out.println(point[i]);
+        // }
+        
+        //결과 계산
+        for(int i=0;i<n;i++){
+            
+            int gift = 0;
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==j) continue;
+                
+                int a = list[i][j];
+                int b = list[j][i];
+                
+                if(a > b){
+                    gift++;
+                }
+                else if((a==0 && b==0)|| a == b){
+                    
+                    if(point[i]>point[j]) gift++;
+                    
+                }
+            }
+            
+            answer = Math.max(gift,answer);
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
