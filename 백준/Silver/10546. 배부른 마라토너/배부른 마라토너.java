d048d37b8a2f7ff5a740a0044dc9b423ca854c6b
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2024-12-21 18:44:32 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 356 ms, Memory: 35304 KB -BaekjoonHub

--- 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -1,37 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(bf.readLine());
 
-        sc.nextLine();
 
         Map<String, Integer> mp = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            String name = sc.nextLine();
+            String name = bf.readLine();
 
             mp.put(name, mp.getOrDefault(name,0)+1);
         }
 
         for (int i = 0; i < n - 1; i++) {
-            String name = sc.nextLine();
-            mp.put(name,mp.get(name)-1);
-            if (mp.get(name) == 0) {
+            String name = bf.readLine();
+            int count = mp.get(name);
+            if (count - 1 == 0) {
                 mp.remove(name);
+            }else{
+                mp.put(name,count-1);
             }
         }
 
-        String answer = "";
         for (String s : mp.keySet()) {
-            answer = s;
+            System.out.println(s);
         }
-        System.out.println(answer);
 
 
 
