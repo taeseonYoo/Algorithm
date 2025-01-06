766dd366967e6d35d8d13bd72d40d57d95eb5d3b
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-01-06 20:45:35 +0900

    [level 2] Title: 피로도, Time: 8.69 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,38 @@
+class Solution {
+    int answer = -1;
+    
+    public int solution(int k, int[][] dungeons) {
+
+        permute(dungeons ,0 ,k);
+        
+        return answer;
+    }
+    
+    public void permute(int[][] dungeons, int p, int k){
+        
+        if(p == dungeons.length){
+            
+            int total = 0;
+            
+            for(int[] dungeon : dungeons){
+                if(dungeon[0] <= k){
+                    total++;
+                    k -= dungeon[1];
+                }
+            }
+            answer = answer<total ? total : answer; 
+            
+        }
+        
+        for(int i=p;i<dungeons.length;i++){
+            swap(dungeons,p,i);
+            permute(dungeons, p+1,k);
+            swap(dungeons,p,i);
+        }
+    }
+    public void swap(int[][] dungeons,int i,int j){
+        int[] temp = dungeons[i];
+        dungeons[i] = dungeons[j];
+        dungeons[j] = temp;
+    }
+}
