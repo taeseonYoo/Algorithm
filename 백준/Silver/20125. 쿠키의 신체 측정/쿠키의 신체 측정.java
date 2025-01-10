0dc0f0946621f557b680b857b38328ce3b2498ab
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-01-10 17:19:41 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 304 ms, Memory: 23084 KB -BaekjoonHub

--- 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
@@ -6,7 +6,6 @@ public class Main {
 
     static int n;
     static char[][] ch;
-    static boolean[][]visited;
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -26,49 +25,50 @@ public class Main {
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, 1, -1};
 
+        //심장의 위치 기록
         int hx = 0;
         int hy = 0;
 
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                boolean flag = false;
+                boolean isHeart = true;
                 if (ch[i][j] == '*') {
                     for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx <= n && nx >= 1 && ny <= n && ny >= 1) {
-                            if (ch[nx][ny] != '*') {
-                                flag = false;
-                                break;
-                            } else {
-                                flag = true;
-                            }
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+                        if (x > n || x < 1 || y > n || y < 1 || ch[x][y]!='*') {
+                            isHeart = false;
                         }
                     }
-                    if (flag) {
-                        hx = i;
-                        hy = j;
-                        break;
-                    }
+                } else isHeart = false;
+
+                if (isHeart) {
+                    hx = i;
+                    hy = j;
+                    break;
                 }
             }
         }
-        int[] s = new int[5];
 
-        visited = new boolean[n + 1][n + 1];
+        int[] result = new int[5];
+
 
         int count = 1;
 
-        s[0] = dfs(hx, hy-1, 0, -1, count);
-        s[1] = dfs(hx, hy + 1, 0, 1, count);
-        s[2] = dfs(hx + 1, hy, 1, 0, count);
-        s[3] = dfs(hx + s[2] + 1, hy - 1, 1, 0, count);
-        s[4] = dfs(hx + s[2] + 1, hy + 1, 1, 0, count);
+        result[0] = dfs(hx, hy - 1, 0, -1, count);
+        result[1] = dfs(hx, hy + 1, 0, 1, count);
+        result[2] = dfs(hx + 1, hy, 1, 0, count);
+        result[3] = dfs(hx + result[2] + 1, hy - 1, 1, 0, count);
+        result[4] = dfs(hx + result[2] + 1, hy + 1, 1, 0, count);
+
+
 
         System.out.println(hx+" "+hy);
+
         for (int i = 0; i < 5; i++) {
-            System.out.print(s[i]+" ");
+            System.out.print(result[i]+" ");
         }
+
     }
 
 
@@ -77,16 +77,11 @@ public class Main {
         int x = sx + tx;
         int y = sy + ty;
 
-        // 범위를 벗어나거나 조건에 맞지 않으면 현재 count 반환
-        if (x < 1 || x > n || y < 1 || y > n || ch[x][y] != '*' || visited[x][y]) {
-            return count;
+        if (x < 1 || x > n || y < 1 || y > n || ch[x][y] != '*') {
+            return count; //종료되는 포인트를 찾으면 그 때 count 반환
         }
 
-        visited[x][y] = true; // 방문 처리
-
-        // 재귀 호출 결과를 받아서 count를 갱신
         return dfs(x, y, tx, ty, count + 1);
-
     }
 
 }
