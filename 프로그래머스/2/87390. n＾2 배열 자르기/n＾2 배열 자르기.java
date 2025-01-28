04ace2db202d414eae45db7fa0bd051695511ef2
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-01-28 17:52:56 +0900

    [level 2] Title: n^2 배열 자르기, Time: 15.43 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,42 +1,17 @@
 import java.util.*;
 
 class Solution {
-    public List<Long> solution(int n, long left, long right) {
+    public List<Integer> solution(int n, long left, long right) {
         
-        List<Long> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        long startRow = left / n ; 
-        long startColumn = left % n ; 
-        
-        long endRow = right / n ; 
-        
-        for(long i=startColumn;i<n;i++){
-            if(list.size()==(right-left+1)){
-                break;
-            }
-            if(startRow<i){
-                list.add(i+1);
-            }else{
-                list.add(startRow+1);
-            }
-        }
-        startRow++;
-        
-        while(startRow <= endRow){
+        for(long i=left;i<=right;i++){
             
-            for(long i=0;i<n;i++){
-                
-                if(list.size()==(right-left+1)){
-                    break;
-                }
-                
-                if(startRow<i){
-                    list.add(i+1);
-                }else{
-                    list.add(startRow+1);
-                }
+            if(i/n <= i%n){
+                list.add((int)(i%n+1));
+            }else{
+                list.add((int)(i/n +1));
             }
-            startRow++;
         }
         
         return list;
