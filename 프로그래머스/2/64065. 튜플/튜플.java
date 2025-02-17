26cf4d014f87af74a139e483b2f3ff1210eb5827
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-02-17 13:25:59 +0900

    [level 2] Title: 튜플, Time: 58.58 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,55 +1,22 @@
 import java.util.*;
-
 class Solution {
-    public List<Integer> solution(String s) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(String s) {
         
+        Set<String> set = new HashSet<>();
         
-        s = s.replace("},{"," ");
-        s = s.substring(2,s.length()-2);
-        String[] sa = s.split(" ");
+        String[] sa = s.replace("{", " ").replaceAll("}", " ").trim().split(" , ");
         
+        Arrays.sort(sa, (a, b)->{return a.length() - b.length();});
         
+        int[] answer = new int[sa.length];
         
-        Arrays.sort(sa, new Comparator<String>(){
-            @Override
-            public int compare(String o1,String o2){
-                if(o1.length()<o2.length()){
-                    return -1;
-                }else{
-                    return 1;
-                }
-            }
-        });
+        int idx = 0;
         
-        // for(int i=0;i<sa.length;i++){
-        //     System.out.println(sa[i]);
-        // }
-        
-        
-        answer.add(Integer.parseInt(sa[0]));
-        
-        for(int i=1;i<sa.length;i++){
-            String[] tmp = sa[i].split(",");
-            
-            for(int j=0;j<answer.size();j++){
-                
-                for(int k=0;k<tmp.length;k++){
-                    if(Integer.parseInt(tmp[k])==answer.get(j)){
-                        tmp[k]="0";
-                        break;
-                    }
-                }
-            }
-            for(int j=0;j<tmp.length;j++){
-                if(!tmp[j].equals("0")){
-                    answer.add(Integer.parseInt(tmp[j]));
-                    break;
-                }
+        for(String s1 : sa) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
         }
-        
-        
         return answer;
     }
 }
