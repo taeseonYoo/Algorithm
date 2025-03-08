108acfc3855727d5fd714dc3d07c4a0c0411c11c
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-03-08 18:20:59 +0900

    [level 3] Title: 야근 지수, Time: 123.05 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0;i<works.length;i++){
+            pq.offer(works[i]);
+        }
+        
+        while(n>0){
+            Integer num = pq.poll();
+            num = num > 0 ? num-1 : 0;
+            pq.offer(num);
+            n--;
+        }
+        
+        for(int i=0;i<works.length;i++){
+            answer += (long)Math.pow(pq.poll(),2);
+        }
+        
+        return answer;
+    }
+}
