3d8604421276f218824c140bf8512de65cf286d1
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-04-11 21:40:47 +0900

    [Silver V] Title: 올림픽, Time: 96 ms, Memory: 14028 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] agrs) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer[]> medal = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+
+            medal.add(new Integer[]{num, gold, silver, bronze});
+        }
+
+        Collections.sort(medal, new Comparator<>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                if(o1[1]<o2[1]) return 1;
+                else if (o1[1].equals(o2[1])) {
+                    if(o1[2]<o2[2]) return 1;
+                    else if (o1[2].equals(o2[2])) {
+                        if(o1[3]<o2[3]) return 1;
+                        else return -1;
+                    }else return -1;
+                }else return -1;
+            }
+        });
+
+
+
+        int rank = 1;
+
+        for (int i = 1; i < medal.size(); i++) {
+
+            Integer[] before = medal.get(i-1);
+            Integer[] next = medal.get(i);
+
+            if(before[1].equals(next[1])&&
+                    before[2].equals(next[2])&&
+                    before[3].equals(next[3])){
+
+            }else{
+                rank++;
+            }
+
+            if (k == next[0]) {
+                System.out.println(rank);
+                break;
+            }
+        }
+
+
+    }
+
+
+}
+
+
