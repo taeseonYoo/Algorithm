c217975fef46c8e57392c335e24b661abf7b728c
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-04-13 21:57:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,18 +1,9 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
+        int answer = 45;
         
-        for(int i=1;i<10;i++){
-            Boolean flag = false;
-            for(int n:numbers){
-                if(n==i){
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag){
-                answer+=i;
-            }
+        for(int n:numbers){
+            answer -= n;
         }
         return answer;
     }
