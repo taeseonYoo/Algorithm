400c4d14e4f95c9b601c775ccab04717520b6f87
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-02 23:06:09 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,39 @@
 class Solution {
     
+    //정답
     int answer = 51;
     
     public int solution(String begin, String target, String[] words) {
+        //방문을 확인하기 위한 배열
+        boolean[] visited = new boolean[words.length];
         
-        dfs(begin,target,words,0);
+        dfs(begin,target,words,0,visited);
         
+        // 변환할 수 없는 경우에는 0을 리턴한다.
         return answer = answer == 51 ? 0 : answer;
     }
-    public void dfs(String begin,String target,String[] words,int depth){
+    
+    public void dfs(String begin,String target,String[] words,int depth,boolean[] visited){
         
         if(begin.equals(target)){
             answer = Math.min(answer,depth);
+            return;
         }
         
         for(int i=0;i<words.length;i++){
-            int count = 0;
+            
+            int diff = 0;
+            // begin과 타겟 word가 한글자만 다른 경우를 확인한다.
             for(int j=0;j<begin.length();j++){
-                if(begin.charAt(j) == words[i].charAt(j)){
-                    count++;
+                if(begin.charAt(j) != words[i].charAt(j)){
+                    diff++;
                 }
             }
-            if(count == begin.length()-1){
-                String[] wo = new String[words.length-1];
-                int inx = 0;
-                for(int k=0;k<words.length;k++){
-                    if(k==i){
-                        continue;
-                    }
-                    wo[inx++] = words[k];
-                }
-            
-                dfs(words[i],target,wo,depth+1);
+            //한글자만 다르고, 타겟 word를 사용한 적이 없는 경우를 확인한다.
+            if(diff ==1 && !visited[i]){
+                visited[i]=true;
+                dfs(words[i],target,words,depth+1,visited);
+                visited[i]=false;
             }
         }
     }
