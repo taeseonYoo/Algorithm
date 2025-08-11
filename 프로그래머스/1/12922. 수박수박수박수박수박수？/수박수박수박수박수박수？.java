cc36511fd2a5e8c014ba59b45f12326b8aef7b3a
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-11 12:00:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.01 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,9 +2,8 @@ class Solution {
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
         
-        for(int i=0;i<n;i++){
-            if(i%2==0) sb.append("수");
-            else sb.append("박");
+        for(int i=1;i<=n;i++){
+            sb.append(i%2==0 ? "박" : "수");
         }
         
         return sb.toString();
