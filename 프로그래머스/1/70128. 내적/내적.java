07ed5f076ef01eaf04e20ad92da29ce5852a5a67
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-11 11:55:19 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,6 +1,10 @@
+/**
+* 값을 저장하는 answer에는 최대 10억이 저장되므로 int 범위 내에서 가능하다.
+**/
+
 class Solution {
-    public long solution(int[] a, int[] b) {
-        long answer = 0;
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
         
         for(int i=0;i<a.length;i++){
             answer += a[i]*b[i];
