2eb5d0783434d8e3a7b52f1a9f02df2786617df1
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-14 14:02:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -5,24 +5,13 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         // 1. 3진법으로 변환 + sb에 역순으로 저장
-        long num = n;
-        
-        while(num>0){
-            sb.append(num%3);
-            num /= 3;
+        while(n>0){
+            sb.append(n%3);
+            n /= 3;
         }
-        
-        num = Long.parseLong(sb.toString());
-        
         
         // 2. 10진법으로 변환
-        String target = String.valueOf(num);
-        
-        for(int i=0;i<target.length();i++){
-            char bit = target.charAt(i);
-            
-            answer += Math.pow(3,target.length()-i-1)* (bit - '0');
-        }
+        answer = Integer.parseInt(sb.toString(),3);
         
         return answer;
     }
