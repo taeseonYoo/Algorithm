51f54db04e5bc6ca23e634c7f116a7f46bd7deb1
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-22 13:26:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.87 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,19 +1,25 @@
 import java.util.*;
 
+/**
+* 1. 아이디어 
+우선순위 큐를 사용해서 자동으로 정렬되도록 한다.
+*/
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         
         //명예의 전당 리스트
-        List<Integer> own = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        int i=0;
-        for(int s:score){
-            own.add(s);
+        for(int i=0;i<score.length;i++){
+            //명예의 전당에 스코어를 입력한다.
+            pq.add(score[i]);
             
-            Collections.sort(own);
-            //명예의 전당에서 k번째 값을 찾는다.
-            answer[i++] = own.size() < k ? own.get(0) : own.get(own.size()-k);
+            //상위 k보다 인원이 많다면 가장 score가 적은 기록을 제거한다.
+            if(pq.size() > k){
+                pq.remove();
+            }
+            answer[i] = pq.peek();
         }
         
         return answer;
