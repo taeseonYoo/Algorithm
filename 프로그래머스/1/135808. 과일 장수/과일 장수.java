5ee8814fea61e1a315c42d3703563a180e6ee895
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-08-26 12:39:27 +0900

    [level 1] Title: 과일 장수, Time: 54.55 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,23 +1,20 @@
 import java.util.*;
+
+/**
+k는 최대 사과품질
+m은 포장 사과 개수
+사과 품질을 정렬해서, 좋은 품질부터 담으면 최대값
+*/
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         
-        //점수 저장
         Arrays.sort(score);
         
-        int count = 0;
-        for(int i=score.length-1;i>=0;i--){
-            
-            count++;
-            if(count == m){
-                answer += score[i] * m ;
-                count = 0;
-            }
+        for(int i=score.length-m;i>=0;i-=m){
+            answer += score[i] * m ;
         }
-        
-        
-        
+           
         return answer;
     }
 }
