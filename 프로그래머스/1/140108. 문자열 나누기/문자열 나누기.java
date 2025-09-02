b280719ce50f21d4b303b8ce989aa8e9ae7ac9d5
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-09-02 20:03:53 +0900

    [level 1] Title: 문자열 나누기, Time: 0.78 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,18 +2,20 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        String tmp = s;
-        
+        //문자열 s의 인덱스를 나타냄.
         int idx = 0;
-        while(idx < tmp.length()){
-            int xNum = 0;
+        
+        while(idx < s.length()){
+            //xNum은 첫글자, oNum은 첫글자와 다른 글자의 수
+            int xNum = 1;
             int oNum = 0;
             
-            char x = tmp.charAt(idx++);
-            xNum++;
+            //첫 글자 = x
+            char x = s.charAt(idx++);
             
-            while(xNum != oNum && idx<tmp.length()){
-                char target = tmp.charAt(idx++);
+            while(xNum != oNum && idx<s.length()){
+                //첫 글자 다음 숫자
+                char target = s.charAt(idx++);
                 if(target==x){
                     xNum++;
                 }else{
