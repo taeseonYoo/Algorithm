0eaf05634a83f87dd47a756de2c30e3fdc1eb490
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-09-08 21:08:11 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.93 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061#) 
 
 ### 성능 요약
 
-메모리: 89.3 MB, 시간: 0.93 ms
+메모리: 80.5 MB, 시간: 0.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 08일 21:03:34
+2025년 09월 08일 21:08:09
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -7,29 +7,24 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         
         for(int move : moves){
-            
-            int target = 0;
-            //목표물을 집는다.
             for(int i=0;i<board.length;i++){
-                target = board[i][move-1];
+                int target = board[i][move-1];
                 if(target != 0){
+                    
+                    if(st.isEmpty()){
+                        st.push(target);
+                    }else if(st.peek()==target){
+                        answer+=2;
+                        st.pop();
+                    }else{
+                        st.push(target);
+                    }
                     board[i][move-1] = 0;
                     break;
                 }
             }
             
-            //집을 인형이 없다면
-            if(target == 0){
-                continue;
-            } 
             
-            //바구니의 가장 위의 인형과 비교한다.
-            if(!st.isEmpty() && st.peek()==target){
-                answer+=2;
-                st.pop();
-            }else{
-                st.push(target);
-            }
         }
         
         
