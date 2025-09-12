723510d4e42cb736a1ce3ff76482465e155fd900
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-09-12 17:23:26 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.50 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,57 +1,56 @@
-import java.util.*;
-
-/**
-키패드를 숫자로 변환한다. ex
-1 2 3
-4 5 6
-7 8 9
-10 11 12
-*/
-
 class Solution {
+    
+    int[][] keyPad = {
+        {3,1}, //0
+        {0,0}, //1
+        {0,1}, //2
+        {0,2}, //3
+        {1,0}, //4
+        {1,1}, //5
+        {1,2}, //6
+        {2,0}, //7
+        {2,1}, //8
+        {2,2}  //9
+    };
     
     public String solution(int[] numbers, String hand) {
         StringBuilder answer = new StringBuilder();
         
-        int left = 10;
-        int right = 12;
+        int[] left = {3,0};
+        int[] right = {3,2};
         
-        for(int i=0;i<numbers.length;i++){
-            int target = numbers[i];
+        
+        for(int target : numbers){
             
+            //왼쪽 키패드
             if(target == 1 || target == 4 || target == 7){
-                left = target;
+                left=keyPad[target];
                 answer.append("L");
             }else if(target == 3 || target == 6 || target == 9){
-                right = target;
+                right = keyPad[target];
                 answer.append("R");
-            }else if(target == 2 || target == 5 || target == 8 || target == 0){
-                if(target == 0) target = 11;
-                
-                int leftDiff = Math.abs(left-target)%3 + Math.abs(left-target)/3;
-                int rightDiff = Math.abs(right-target)%3 + Math.abs(right-target)/3;
-                System.out.println(target+" "+leftDiff+" "+rightDiff);
-                
-                if(leftDiff < rightDiff){
-                    left = target;
+            }else{
+                if(distinct(left,target)<distinct(right,target)){
+                    left = keyPad[target];
                     answer.append("L");
-                }else if(leftDiff > rightDiff){
-                    right = target;
+                }else if(distinct(left,target)>distinct(right,target)){
+                    right = keyPad[target];
                     answer.append("R");
                 }else{
                     if("left".equals(hand)){
-                        left = target;
+                        left = keyPad[target];
                         answer.append("L");
-                    }else if("right".equals(hand)){
-                        right = target;
+                    }else{
+                        right = keyPad[target];
                         answer.append("R");
                     }
                 }
             }
         }
-        
-        
-            
         return answer.toString();
+    }
+    //거리를 측정하는 메서드
+    public int distinct(int[] pos,int target){
+        return Math.abs(pos[0]-keyPad[target][0]) + Math.abs(pos[1]-keyPad[target][1]);
     }
 }
