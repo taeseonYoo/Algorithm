2ee0fbdc1d44a88020c1e51d509e4357ab3f17c2
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-09-22 17:17:45 +0900

    [level 1] Title: 유연근무제, Time: 0.58 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -3,44 +3,42 @@ class Solution {
         int answer = 0;
         
         for(int i=0;i<schedules.length;i++){
+            boolean isLate = false;
             
-            int hour = schedules[i]/100;
-            int minute = schedules[i]%100;
-            minute += 10;
-            
-            if(minute >=60){
-                hour += 1;
-                minute -= 60;
-            }
-            
-            int day = startday-1;
-            boolean flag = false;
+            schedules[i] = calculateTime(schedules[i]);
             
             for(int j=0;j<timelogs[i].length;j++){
+                //현재 요일을 나타낸다. 6이면 토요일 0이면 일요일
+                int currentDay = (startday+j) % 7; 
+                
                 //주말은 무시한다.
-                if(day == 5 || day == 6) {
-                    day = (day+1)%7;
-                    continue;
-                }
-                int rHour = timelogs[i][j]/100;
-                int rMinute = timelogs[i][j]%100;
-                
-                if( rHour == hour && rMinute > minute){
-                    flag = true;
-                    break;
-                }else if(rHour > hour){
-                    flag = true;
+                if(currentDay == 0 || currentDay == 6) continue;
+
+                //출근 희망 시간에 늦는다면, 탐색을 종료
+                if(timelogs[i][j] > schedules[i]) {
+                    isLate = true;
                     break;
                 }
-                day = (day+1)%7;
             }
-            if(!flag) {
+            
+            if(!isLate) {
                 answer++;
-                
             }
+            
         }
-        
-        
         return answer;
     }
+    
+    public int calculateTime(int time){
+        int hour = time/100;
+        int minute = time%100;
+        minute += 10;
+            
+        if(minute >=60){
+            hour += 1;
+            minute -= 60;
+        }
+        return hour*100 + minute;
+    }
+    
 }
