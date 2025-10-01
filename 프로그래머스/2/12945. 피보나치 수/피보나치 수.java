529e3cad44891e91c72e3ea0cd55050bcdeacec2
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-10-01 15:47:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.40 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        int a = 0, b = 1;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        for(int i = 2; i<=n; i++){
-            answer = (a + b)%1234567;
-            a = b;
-            b = answer;
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) %1234567;
         }
-        return answer;
+        return dp[n];
     }
+
 }
