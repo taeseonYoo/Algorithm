e224c2d6e24536cff4b9f91a421c07e6da6a267b
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-10-21 11:29:11 +0900

    [level 2] Title: [3차] n진수 게임, Time: 13.22 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,23 +2,18 @@ class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder answer = new StringBuilder();
         
+        StringBuilder sb = new StringBuilder();
+        
         int num = 0;
-        int turn = 1;
-        while(answer.length() < t){
-            
+        while(sb.length() < t * m){
             String result = Integer.toString(num++,n).toUpperCase();
-            
-            //튜브의 순서라면 더한다.
-            for(int j=0;j<result.length();j++){
-                //현재 차례
-                if(turn == p){
-                    answer.append(result.charAt(j));
-                    if(answer.length() == t) break;
-                }
-                turn++;
-                if(turn == m+1) turn = 1;
-            }
-            
+            sb.append(result);
+        }
+        
+        int index = p-1;
+        while(t-- >0){
+            answer.append(sb.charAt(index));
+            index += m;
         }
         
         
