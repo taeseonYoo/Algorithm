aac79b63c1f1281f4fd6c256d691f044fdc69822
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-12-11 22:56:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.31 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,38 +1,41 @@
 import java.util.*;
 
 class Solution {
+    boolean[][] edges;
+    boolean[] visited;
     public int solution(int n, int[][] wires) {
         int answer = n;
         
-        ArrayList<ArrayList<Integer>> network = new ArrayList<>();
+        edges = new boolean[n+1][n+1];
         
-        for(int i=0;i<=n;i++){
-            network.add(new ArrayList<Integer>());
-        }
         
         for(int[] wire : wires){
             int start = wire[0];
             int end = wire[1];
-            network.get(start).add(end);
-            network.get(end).add(start);
+            edges[start][end] = true;
+            edges[end][start] = true;
         }
         
         for(int i=0;i<wires.length;i++){
             int[] wire = wires[i];
             int start = wire[0];
             int end = wire[1];
-            boolean[] visited = new boolean[n+1];
-            network.get(start).remove(Integer.valueOf(end));
-            network.get(end).remove(Integer.valueOf(start));
-            answer = Math.min(answer,Math.abs(n-2*bfs(1,visited,network)));
-            network.get(start).add(Integer.valueOf(end));
-            network.get(end).add(Integer.valueOf(start));
+            visited = new boolean[n+1];
+            
+            edges[start][end] = false;
+            edges[end][start] = false;
+            
+            int count = bfs(1);
+            answer = Math.min(answer,Math.abs((n-count)-count));
+            
+            edges[start][end] = true;
+            edges[end][start] = true;
         }
         
         return answer;
     }
     
-    int bfs(int start,boolean[] visited ,ArrayList<ArrayList<Integer>> network){
+    int bfs(int start){
         
         Queue<Integer> q = new LinkedList<>();
         
@@ -44,11 +47,10 @@ class Solution {
         while(!q.isEmpty()){
             int poll = q.poll();
             
-            for(int i=0;i<network.get(poll).size();i++){
-                int target = network.get(poll).get(i);
-                if(!visited[target]) {
-                    visited[target] = true;
-                    q.offer(target);
+            for(int i=1;i<edges.length;i++){
+                if(edges[poll][i] && !visited[i]){
+                    visited[i] = true;
+                    q.offer(i);
                     count++;
                 }
             }
