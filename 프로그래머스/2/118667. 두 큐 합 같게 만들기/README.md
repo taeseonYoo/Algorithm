94859630aa6b8cacfcb93d50454292f1f7e535ed
taeseonYoo <74961371+taeseonYoo@users.noreply.github.com>
2025-12-14 00:51:16 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 14.41 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
 
 ### 성능 요약
 
-메모리: 120 MB, 시간: 14.59 ms
+메모리: 124 MB, 시간: 14.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 14일 00:27:28
+2025년 12월 14일 00:51:14
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,59 +1,42 @@
-// 두 큐의 원소들의 값을 더한다.
-// 중간 값을 찾는다.
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
+        int[] totalQueue = new int[queue1.length + queue2.length];
         
-        long sum = 0;
+        long queue1Sum = 0;
+        long queue2Sum = 0;
         
         for(int i=0;i<queue1.length;i++){
-            sum += queue1[i];
+            int num = queue1[i];
+            totalQueue[i] = num;
+            queue1Sum += num;
         }
-        for(int j=0;j<queue2.length;j++){
-            sum += queue2[j];
+        
+        for(int i=queue1.length;i<queue1.length+queue2.length;i++){
+            int num = queue2[i-queue1.length];
+            totalQueue[i] = num;
+            queue2Sum += num;
         }
         
-        if(sum %2 !=0 )return -1;
+        //각 큐의 합을 중간 값으로 나눌 수 없다면 -1을 반환한다.
+        if((queue1Sum + queue2Sum)%2 !=0) return -1;
         
-        long middle = sum/2;
+        int left = 0;
+        int right = queue1.length;
+        long middle = (queue1Sum + queue2Sum) /2;
         
-        int startIdx = 0;
-        int endIdx = queue1.length;
-        long partSum = 0;
-        for(int i=0;i<queue1.length;i++){
-            partSum += queue1[i];
-        }
-    
-        
-        boolean flag = false;
-        
-        while(startIdx < queue1.length + queue2.length){
+        while(right < totalQueue.length){
             
-            if(partSum<middle){
-                if(endIdx < queue1.length){
-                    partSum += queue1[endIdx];
-                }else{
-                    partSum += queue2[endIdx-queue1.length];
-                }
-                endIdx++;
-                if(endIdx >= queue1.length + queue2.length) endIdx = 0;
-            }else if(partSum > middle){
-                if(startIdx < queue1.length){
-                    partSum -= queue1[startIdx];
-                }else{
-                    partSum -= queue2[startIdx-queue1.length];
-                }
-                startIdx++;
+            if(queue1Sum == middle){
+                return answer;
+            }else if(queue1Sum > middle){
+                queue1Sum -= totalQueue[left++];
             }else{
-                flag = true;
-                break;
+                queue1Sum += totalQueue[right++];
             }
             answer++;
         }
         
-        if(!flag) answer = -1;
-        
-        return answer;
+        return -1;
     }
 }
